72adb1350f321ff318e399e20d1aafdb22484b38
yaya <55609403+Felia-Rhafida@users.noreply.github.com>
2022-08-27 10:16:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function checkForm(){
+    var Nama = document.getElementById("Nama").value;
+    var Aduan = document.getElementById("Aduan").value;
+
+    document.write("Hasil Form Pengaduan <br>");
+    document.write("Nama : "+ Nama + "<br>");
+    document.write("Aduan : "+ Aduan + "<br>");
+}
